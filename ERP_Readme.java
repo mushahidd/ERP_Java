99acd7f52819bd86d6344ecb47349360b14949f1
Mushahid Hussain <muhammadmushahid.cs@gmail.com>
2024-12-20 02:20:11 +0500

    Update ERP_Readme.java

--- ERP_Readme.java
+++ ERP_Readme.java
@@ -1,19 +1,21 @@
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("A semester project (PMS) using Java and MySql
+        System.out.println("A semester project (ERP) using Java and MySql
 
--> A very simple project in java which is Project Management System in which we can upload our projects and can save them and also can download the saved project with CRUD functionality, connected with database (MySql) local host phpmyadmin.
+-> A very simple project in java which is Enterprise Resource Planning, connected with database (MySql) local host phpmyadmin.
 
 -> First you should have these 2 software "Netbeans & XAMPP" to run this project.
 
+-> Now download the .rar file above (all files are in .rar folder no need to download the readme.java file).
+
 -> Now start XAMPP and run Apache with MySql.
 
--> Now go to web http://localhost/phpmyadmin and then create a database name dbms (i dont remember that what to name the DB but if dbms doesn't works so go to the source package then dao folder then in "connection provider" file in which you can see what to name the database) after creating database in phpmyadmin follow next step.
+-> Now go to web http://localhost/phpmyadmin and then create a database name erp after creating database in phpmyadmin follow next step.
 
--> Now go to source package then in (dao) folder then go to "table file" then uncomment all the lines which are commented then run the table file once, recomment those lines after running the file.
+-> Now go to source package then in (dao) folder go to table file then uncomment all the lines which are commented then run the table file once, recomment those lines after running the file.
 
--> Then run the project and login by entering (admin12) both in Id & password. (If admin12 does not work try admin)
+-> Then run the project and login by entering (admin) both in Id & password.
 
 -> Now explore the functionality of this project and enjoy.
 
